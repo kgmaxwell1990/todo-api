b47cc56663d7a86ea9ce85ef494b15b75454b228
Katie Maxwell <katie@codeinstitute.net>
2018-07-10 13:42:12 +0000

    switched to heroku postgres database

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -79,10 +79,7 @@ WSGI_APPLICATION = 'todo_api.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"))
 }
 
 
